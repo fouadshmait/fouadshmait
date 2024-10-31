7379e7515dd2d13d132dffd6175b3f7a1b45b176
Fouad Shmait <162714250+fouadshmait@users.noreply.github.com>
2024-10-31 15:35:40 +0200

    Delete FouadShmait_Sum of Odd Numbers_main.py

--- FouadShmait_Sum of Odd Numbers_main.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Enter a number: "))
-sum = 0  # Initialize sum outside the loop
-
-for i in range(1, number + 1):
-  if(i%2!=0):
-    sum += i  # Add i to sum
-
-print("The sum of numbers from 1 to", number, "is:", sum)
-  
